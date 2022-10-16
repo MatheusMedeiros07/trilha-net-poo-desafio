a90b71436e03ce6c0725e8d932d73d738270b49e
Matheus Medeiros <matheusmedeiroswf@gmail.com>
2022-10-15 21:13:39 -0300

    Resolução do Desafio Proposto

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -3,22 +3,26 @@ namespace DesafioPOO.Models
     public abstract class Smartphone
     {
         public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
+        private string Modelo { get; set; }
+        private string IMEI { get; set; }
+        private int Memoria { get; set; }
 
-        public Smartphone(string numero)
+        public Smartphone(string numero, string modelo, string imei, int memoria)
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
         }
 
-        public void Ligar()
+        public void Ligar(string numero)
         {
-            Console.WriteLine("Ligando...");
+            Console.WriteLine($"Ligando para o número {numero}.");
         }
 
-        public void ReceberLigacao()
+        public void ReceberLigacao(string numero)
         {
-            Console.WriteLine("Recebendo ligação...");
+            Console.WriteLine($"Recebendo ligação do número {numero}.");
         }
 
         public abstract void InstalarAplicativo(string nomeApp);
--- Program.cs
+++ Program.cs
@@ -1,3 +1,20 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("\nSmarthphone iPhone...");
+
+Iphone iphone1 = new Iphone("89232343", "iPhone 14 pro Max", "Cy378383", 8);
+Console.WriteLine($"O número do Smarthphone é: { iphone1.Numero}");
+iphone1.Ligar("(11)95154-0405");
+iphone1.ReceberLigacao("(11)95154-0405");
+iphone1.InstalarAplicativo("WhatsApp");
+iphone1.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\nSmarthphone Nokia...");
+
+Nokia nokia1 = new Nokia("62847390", "Nokia Ilumia", "357t85ba43", 6);
+Console.WriteLine($"O número do Smarthphone é: { nokia1.Numero}");
+nokia1.Ligar("(11)90504-4515");
+nokia1.ReceberLigacao("(11)90504-4515");
+nokia1.InstalarAplicativo("Instagram");
+nokia1.InstalarAplicativo("Telegram");
+nokia1.InstalarAplicativo("TikTok");
